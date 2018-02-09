bc7b2099c9692b795254ff04e6e758d01b68bf62
brandoncold <coldbutkindxd@gmail.com>
2018-02-08 18:43:16 -0800

    Decks can now be built

--- Card.cs
+++ Card.cs
@@ -17,6 +17,86 @@ namespace farstone
         name = nameVal;
         }
     }
+    public class Wisp : Minion{
+        public Wisp():base(1,1,0,"Wisp"){
+        }
+    }
+    public class Tree : Minion{
+        public Tree():base(1,1,0,"Tree"){
+        }
+    }
+    public class DireMole : Minion{
+        public DireMole():base(1,3,1,"DireMole"){
+        }
+    }
+    public class MurlocRaider : Minion{
+        public MurlocRaider():base(2,1,1,"Murloc"){
+        }
+    }
+    public class BloodfenRaptor : Minion{
+        public BloodfenRaptor():base(3,2,2,"Raptor"){
+        }
+    }
+    public class AcidicSwampOoze : Minion{
+        public AcidicSwampOoze():base(3,2,2,"AcidOoze"){
+        }
+    }
+    public class Duskboar : Minion{
+        public Duskboar():base(4,1,2,"Duskboar"){
+        }
+    }
+    public class RiverCrocolisk : Minion{
+        public RiverCrocolisk():base(2,3,2,"Croclisk"){
+        }
+    }
+    public class IronfurGrizzly : Minion{
+        public IronfurGrizzly():base(3,3,3,"Grizzly"){
+        }
+    }
+    public class MagmaRager : Minion{
+        public MagmaRager():base(5,1,3,"MagmaMan"){
+        }
+    }
+    public class SquirmingTentacle : Minion{
+        public SquirmingTentacle():base(2,4,3,"Tentacle"){
+        }
+    }
+    public class FireFly : Minion{
+        public FireFly():base(3,6,4,"FireFly"){
+        }
+    }
+    public class ChillwindYeti : Minion{
+        public ChillwindYeti():base(4,5,4,"ChillYeti"){
+        }
+    }
+    public class AncientBrewmaster : Minion{
+        public AncientBrewmaster():base(5,4,4,"Ancient"){
+        }
+    }
+    public class SpitefulSmith : Minion{
+        public SpitefulSmith():base(4,6,5,"Smith"){
+        }
+    }
+    public class Sergeant : Minion{
+        public Sergeant():base(5,5,5,"Sergeant"){
+        }
+    }
+    public class BoulderfistOgre : Minion{
+        public BoulderfistOgre():base(6,7,6,"Ogre"){
+        }
+    }
+    public class StoneMan : Minion{
+        public StoneMan():base(7,6,6,"Ogre"){
+        }
+    }
+    public class CoreHound : Minion{
+        public CoreHound():base(9,5,7,"Hound"){
+        }
+    }
+    public class LavaMan : Minion{
+        public LavaMan():base(8,8,8,"LavaMan"){
+        }
+    }
     public class Spell : Card{
         Spell(int costVal, string nameVal){
         cost = costVal;
--- createDeck.cs
+++ createDeck.cs
@@ -8,58 +8,56 @@ namespace farstone
         public static List<Card> MakingDeck()
         {
             List<Card> cardList = new List<Card>();
-            object[,] deckBlueprint = new object[20, 4]
+            List<Card> deckBlueprint = new List<Card>();
             {
-                {1,1,0,"Wisp"},
-                {1,1,0,"Tree"},
-                {1,3,1,"DireMole"},
-                {2,1,1,"Murloc"},
-                {3,2,2,"Raptor"},
-                {3,2,2,"AcidOoze"},
-                {4,1,2,"Duskboar"},
-                {2,3,2,"Croclisk"},
-                {3,3,3,"Grizzly"},
-                {5,1,3,"MagmaMan"},
-                {2,4,3,"Tentacle"},
-                {3,6,4,"FireFly"},
-                {4,5,4,"ChillYeti"},
-                {5,4,4,"Ancient"},
-                {4,6,5,"Smith"},
-                {5,5,5,"Sergeant"},
-                {6,7,6,"Ogre"},
-                {7,6,6,"StoneMan"},
-                {9,5,7,"Hound"},
-                {8,8,8,"LavaMan"},
+                deckBlueprint.Add(new Wisp());
+                deckBlueprint.Add(new Tree());
+                deckBlueprint.Add(new DireMole());
+                deckBlueprint.Add(new MurlocRaider());
+                deckBlueprint.Add(new BloodfenRaptor());
+                deckBlueprint.Add(new AcidicSwampOoze());
+                deckBlueprint.Add(new Duskboar());
+                deckBlueprint.Add(new RiverCrocolisk());
+                deckBlueprint.Add(new IronfurGrizzly());
+                deckBlueprint.Add(new MagmaRager());
+                deckBlueprint.Add(new SquirmingTentacle());
+                deckBlueprint.Add(new FireFly());
+                deckBlueprint.Add(new ChillwindYeti());
+                deckBlueprint.Add(new AncientBrewmaster());
+                deckBlueprint.Add(new SpitefulSmith());
+                deckBlueprint.Add(new Sergeant());
+                deckBlueprint.Add(new BoulderfistOgre());
+                deckBlueprint.Add(new StoneMan());
+                deckBlueprint.Add(new CoreHound());
+                deckBlueprint.Add(new LavaMan());
+
             };
-
-
-            for (int i = 0; i < 20; i++)
-            {
-                Console.WriteLine($"{i} - {deckBlueprint[i,3]} Has {deckBlueprint[i,0]} ATK | {deckBlueprint[i,1]} HP | {deckBlueprint[i,2]} Cost");
-            }
 
             while(cardList.Count < 30)
             {
-                Console.WriteLine("What would you like to add to your deck?");
+                for (int i = 0; i < deckBlueprint.Count; i++)
+                {
+                    Console.WriteLine($"{i} - {deckBlueprint[i].name} Has {(deckBlueprint[i] as Minion).atk} ATK | {(deckBlueprint[i] as Minion).hp} HP | {deckBlueprint[i].cost} Cost");
+                }
+                Console.WriteLine($"What would you like to add to your deck? {cardList.Count}/30");
                 int c = GetInput.GetInt();
-                if(deckBlueprint[c,0] == null){
+                if(c > deckBlueprint.Count){
                     Console.WriteLine("That is not a card!");
                 }
                 else
                 {
                     for (int z = 0; z < 2; z++)
                     {
-                        cardList.Add(new Minion((int)deckBlueprint[c,0],(int)deckBlueprint[c,1],(int)deckBlueprint[c,2],(string)deckBlueprint[c,3]));
+                        cardList.Add(deckBlueprint[c]);
                     }
-
-                    cardList.RemoveAt(c);
+                    deckBlueprint.RemoveAt(c);
                 }
             }
             
             Console.WriteLine("Your Deck!");
             for (int i = 0; i < 30; i++)
             {
-                Console.WriteLine($"Card {i}: {cardList[i].name}");
+                Console.WriteLine($"Card {i+1}: {cardList[i].name}");
             }
             return cardList;
         }
